26e0fc8f2171548dac9a0105a1676e2981f3ae05
rtcamp <rtcamp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-30 06:04:00 +0000

    First Release
    git-svn-id: https://plugins.svn.wordpress.org/rtwidgets/trunk@830025 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = function(grunt) {
+
+    // load all grunt tasks matching the `grunt-*` pattern
+    // Ref. https://npmjs.org/package/load-grunt-tasks
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        
+        // Watch for hanges and trigger compass, jshint, uglify and livereload
+        // Ref. https://npmjs.org/package/grunt-contrib-watch
+        watch: {
+            compass: {
+                files: ['assets/scss/**/*.{scss,sass}'],
+                tasks: ['compass']
+            }
+        },
+        
+        // SCSS and Compass
+        // Ref. https://npmjs.org/package/grunt-contrib-compass
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb',
+                    force: true
+                }
+            }
+        },
+        
+        // Fontello Icons
+        // Ref. https://npmjs.org/package/grunt-fontello
+        fontello: {
+            dist: {
+                options: {
+                    config: 'assets/icon-font/config.json',
+                    fonts: 'assets/icon-font/font',
+                    styles: 'assets/icon-font/css',
+                    scss: true,
+                    force: true
+                }
+            }
+        }
+    });
+
+    // Register Task
+    grunt.registerTask('default', ['fontello', 'watch']);
+};
---
+++ assets/rtwidgets-scripts.js
@@ -0,0 +1,9 @@
+/**
+ * rtWidgets Script Library
+ * 
+ * @since rtWidget 1.0
+ */
+
+jQuery(document).ready(function() {
+    jQuery( '#rtw-login-container' ).tabs();
+});
